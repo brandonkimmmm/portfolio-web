2ac78dce3b599527bcb8cb594f6115bc2e1ae713
brandonkimmmm <bkim2490@gmail.com>
2022-01-16 00:58:52 +0900

    initial typescript structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `portfolio`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+	siteMetadata: {
+		title: `portfolio`,
+		siteUrl: `https://www.yourdomain.tld`
+	},
+	plugins: ['gatsby-plugin-postcss']
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => {
+	return (
+		<main>
+			<title>Home Page</title>
+			<div className='text-8xl font-bold'>GOOD MORNING</div>
+		</main>
+	);
+};
+
+export default IndexPage;
